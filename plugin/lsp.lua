3aaf44dfba6bb2fd23d70ec955cf3247e657e03a
Bryan Valeriano <bryan.valeriano.ext@Bryans-MacBook-Pro-842.local>
2023-07-11 17:10:07 -0300

    fix format eslint

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,15 +1,21 @@
-local lsp = require('lsp-zero')
-lsp.preset('recommended')
+local lsp = require("lsp-zero")
+lsp.preset("recommended")
 
 lsp.set_preferences({
-  set_lsp_keymaps = {omit = {'gd'}}
+	set_lsp_keymaps = { omit = { "gd" } },
+})
+
+lsp.format_on_save({
+	format_opts = {
+		async = false,
+		timeout_ms = 10000,
+	},
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
-  local bind = vim.keymap.set
-  bind('n', 'gd', '<cmd>tab split | lua vim.lsp.buf.definition()<CR>', opts)
+	local opts = { buffer = bufnr, remap = false }
+	local bind = vim.keymap.set
+	bind("n", "gd", "<cmd>tab split | lua vim.lsp.buf.definition()<CR>", opts)
 end)
 
 lsp.setup()
-
--- plugin/null-ls.rc.lua
+++ plugin/null-ls.rc.lua
@@ -1,24 +1,27 @@
 local status, null_ls = pcall(require, "null-ls")
-if (not status) then return end
+if not status then
+	return
+end
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.diagnostics.eslint,
-    null_ls.builtins.completion.spell,
-  },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function() -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-          vim.lsp.buf.format({ bufnr = bufnr })
-        end,
-      })
-    end
-  end,
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.diagnostics.eslint,
+		null_ls.builtins.code_actions.eslint_d,
+		null_ls.builtins.completion.spell,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function() -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
