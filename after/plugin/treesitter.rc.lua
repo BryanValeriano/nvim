eb975ad23c959cf898c4fe84de8ecd0890215fed
Bryan Valeriano <bryan.valeriano.ext@Bryans-MacBook-Pro-842.local>
2023-10-20 12:59:38 -0300

    fix treesitter

--- after/plugin/treesitter.rc.lua
+++ after/plugin/treesitter.rc.lua
@@ -1,34 +1,34 @@
 require("nvim-treesitter.configs").setup({
-	highlight = {
-		enable = true,
-		disable = {},
-	},
-	indent = {
-		enable = true,
-		disable = {},
-	},
-	ensure_installed = {
-		"tsx",
-		"typescript",
-		"toml",
-		"fish",
-		"php",
-		"json",
-		"yaml",
-		"swift",
-		"css",
-		"html",
-		"lua",
-		"c",
-		"cpp",
-		"rust",
-		"java",
-		"prisma",
-		"svelte",
-	},
-	autotag = {
-		enable = true,
-	},
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = true,
+    disable = {},
+  },
+  ensure_installed = {
+    "tsx",
+    "typescript",
+    "toml",
+    "fish",
+    "php",
+    "json",
+    "yaml",
+    "swift",
+    "css",
+    "html",
+    "lua",
+    "c",
+    "cpp",
+    "rust",
+    "java",
+    "prisma",
+    "svelte",
+  },
+  autotag = {
+    enable = true,
+  },
 })
 
 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
