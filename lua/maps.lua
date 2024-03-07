3b94c2d331036137f1e372a49a06f5adfcac8b84
BryanValeriano <bryan.valeriano@uber.com>
2024-03-07 11:07:44 -0300

    remove select all remap

--- lua/maps.lua
+++ lua/maps.lua
@@ -1,37 +1,37 @@
 local keymap = vim.keymap
 
-keymap.set('n', 'x', '"_x')
+keymap.set("n", "x", '"_x')
 
 -- Increment/decrement
-keymap.set('n', '+', '<C-a>')
-keymap.set('n', '-', '<C-x>')
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
 
 -- Delete a word backwards
 -- keymap.set('n', 'dw', 'vb"_d')
 
 -- Select all
-keymap.set('n', '<C-a>', 'gg<S-v>G')
+-- keymap.set('n', '<C-a>', 'gg<S-v>G')
 
 -- Save with root permission (not working for now)
 --vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
 -- New tab
-keymap.set('n', 'te', ':tabedit')
+keymap.set("n", "te", ":tabedit")
 -- Split window
-keymap.set('n', 'es', ':split<Return><C-w>w')
-keymap.set('n', 'ev', ':vsplit<Return><C-w>w')
+keymap.set("n", "es", ":split<Return><C-w>w")
+keymap.set("n", "ev", ":vsplit<Return><C-w>w")
 -- Move window
-keymap.set('n', '<Space>', '<C-w>w')
-keymap.set('', 'eh', '<C-w>h')
-keymap.set('', 'ek', '<C-w>k')
-keymap.set('', 'ej', '<C-w>j')
-keymap.set('', 'el', '<C-w>l')
+keymap.set("n", "<Space>", "<C-w>w")
+keymap.set("", "eh", "<C-w>h")
+keymap.set("", "ek", "<C-w>k")
+keymap.set("", "ej", "<C-w>j")
+keymap.set("", "el", "<C-w>l")
 
 -- Resize window
-keymap.set('n', '<C-w><left>', '<C-w><')
-keymap.set('n', '<C-w><right>', '<C-w>>')
-keymap.set('n', '<C-w><up>', '<C-w>+')
-keymap.set('n', '<C-w><down>', '<C-w>-')
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
 
 -- NvimTree
-keymap.set('n', '<C-n>', '<cmd>:NvimTreeToggle<CR>',{noremap=true})
+keymap.set("n", "<C-n>", "<cmd>:NvimTreeToggle<CR>", { noremap = true })
