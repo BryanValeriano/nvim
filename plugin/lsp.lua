0b92af50ecad5599fcd61dcff60ed01305419ffb
BryanValeriano <bryan.valeriano@uber.com>
2024-03-18 11:16:33 -0300

    add capabilities

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,6 +1,13 @@
 local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
+-- nvim-cmp capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.preselectSupport = true
+capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
+
 lsp.set_preferences({
 	set_lsp_keymaps = { omit = { "gd" } },
 })
@@ -35,6 +42,7 @@ require("mason-lspconfig").setup_handlers({
 	["rust_analyzer"] = function()
 		require("rust-tools").setup({
 			server = {
+				capabilities = capabilities,
 				on_attach = function(_, bufnr)
 					vim.keymap.set("n", "<leader>ca", rust_tools.hover_actions.hover_actions, { buffer = bufnr })
 					print("test Maroto")
@@ -44,21 +52,22 @@ require("mason-lspconfig").setup_handlers({
 	end,
 	["gopls"] = function()
 		require("lspconfig")["gopls"].setup({
-      cmd = {'gopls', '-remote=auto', '-rpc.trace', '-v'},
-		  on_attach = on_attach,
-      init_options = {
-        gofumpt= true
-      },
-      settings = {
-        gopls = {
-          analyses = {
-            unusedparams = true,
-          },
-          staticcheck = true,
-        },
-      },
-    })
-  end,
+			cmd = { "gopls", "-remote=auto", "-rpc.trace", "-v" },
+			on_attach = on_attach,
+			capabilities = capabilities,
+			init_options = {
+				gofumpt = true,
+			},
+			settings = {
+				gopls = {
+					analyses = {
+						unusedparams = true,
+					},
+					staticcheck = true,
+				},
+			},
+		})
+	end,
 })
 
 local rust_tools = require("rust-tools")
