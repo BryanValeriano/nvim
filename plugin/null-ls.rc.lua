991b96ea775caee1ff515a60bf5ae5b08ae83426
bryan.valeriano <bryan.valeriano@uber.com>
2024-03-27 14:26:38 +0000

    change go format to default insted of gofumpt

--- plugin/null-ls.rc.lua
+++ plugin/null-ls.rc.lua
@@ -8,7 +8,8 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.gofumpt,
+		-- null_ls.builtins.formatting.gofumpt,
+		null_ls.builtins.formatting.gofmt,
 		null_ls.builtins.formatting.goimports_reviser,
 		null_ls.builtins.diagnostics.eslint,
 		--null_ls.builtins.formatting.eslint,
