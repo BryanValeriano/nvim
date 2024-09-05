0f0d314d15a4b4a255e784a50654640c272ec941
bryan.valeriano <bryan.valeriano@uber.com>
2024-09-05 18:52:09 +0000

    upate bufferline style

--- after/plugin/bufferline.rc.lua
+++ after/plugin/bufferline.rc.lua
@@ -6,7 +6,7 @@ end
 bufferline.setup({
 	options = {
 		mode = "tabs",
-		separator_style = "slant",
+		separator_style = "thin",
 		always_show_bufferline = false,
 		show_buffer_close_icons = false,
 		show_close_icon = false,
@@ -14,26 +14,26 @@ bufferline.setup({
 		diagnostics = "nvim_lsp",
 	},
 
-	highlights = {
-		separator = {
-			fg = "#073642",
-			bg = "#002b36",
-		},
-		separator_selected = {
-			fg = "#073642",
-		},
-		background = {
-			fg = "#657b83",
-			bg = "#002b36",
-		},
-		buffer_selected = {
-			fg = "#fdf6e3",
-			bold = true,
-		},
-		fill = {
-			bg = "#073642",
-		},
-	},
+	-- highlights = {
+	-- 	separator = {
+	-- 		fg = "#073642",
+	-- 		bg = "#002b36",
+	-- 	},
+	-- 	separator_selected = {
+	-- 		fg = "#073642",
+	-- 	},
+	-- 	background = {
+	-- 		fg = "#657b83",
+	-- 		bg = "#002b36",
+	-- 	},
+	-- 	buffer_selected = {
+	-- 		fg = "#fdf6e3",
+	-- 		bold = true,
+	-- 	},
+	-- 	fill = {
+	-- 		bg = "#073642",
+	-- 	},
+	-- },
 })
 
 vim.keymap.set("n", "<C-p>", "<Cmd>BufferLinePick<CR>", {})
