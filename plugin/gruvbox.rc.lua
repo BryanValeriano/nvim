4f8fbb4b18354a94a00df2956129411f99f83e5b
bryan.valeriano <bryan.valeriano@uber.com>
2025-04-24 19:14:20 +0000

    add bold method gruvbox

--- plugin/gruvbox.rc.lua
+++ plugin/gruvbox.rc.lua
@@ -1,34 +1,34 @@
 -- Default options:
 require("gruvbox").setup({
-	terminal_colors = true, -- add neovim terminal colors
-	undercurl = true,
-	underline = true,
-	bold = true,
-	italic = {
-		strings = true,
-		emphasis = true,
-		comments = true,
-		operators = false,
-		folds = true,
-	},
-	strikethrough = true,
-	invert_selection = false,
-	invert_signs = false,
-	invert_tabline = false,
-	invert_intend_guides = false,
-	inverse = true, -- invert background for search, diffs, statuslines and errors
-	contrast = "", -- can be "hard", "soft" or empty string
-	palette_overrides = {},
-	overrides = {
-		["@lsp.type.interface"] = { fg = "#f0e68c", italic = true },
-		["@lsp.type.function"] = { fg = "#b16286" },
-		["@lsp.type.method.go"] = { fg = "#b16286" },
-		["@method.call"] = { fg = "#b16286" },
-		["@function.method.call.go"] = { fg = "#b16286" },
-		["@function.method.go"] = { fg = "#b16286" },
-	},
-	dim_inactive = false,
-	transparent_mode = true,
+  terminal_colors = true, -- add neovim terminal colors
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    emphasis = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {
+    ["@lsp.type.interface"] = { fg = "#f0e68c", italic = true, bold = true },
+    ["@lsp.type.function"] = { fg = "#b16286", bold = true },
+    ["@lsp.type.method.go"] = { fg = "#b16286", bold = true },
+    ["@method.call"] = { fg = "#b16286", bold = true },
+    ["@function.method.call.go"] = { fg = "#b16286", bold = true },
+    ["@function.method.go"] = { fg = "#b16286", bold = true },
+  },
+  dim_inactive = false,
+  transparent_mode = true,
 })
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd("colorscheme gruvbox")
