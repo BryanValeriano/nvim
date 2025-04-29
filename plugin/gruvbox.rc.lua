a1b520a6dfdff90fa4e2c384ecb457787a5a9217
Bryan <bryan.b.valeriano@gmail.com>
2025-04-28 22:42:09 -0300

    update grubbox colors

--- plugin/gruvbox.rc.lua
+++ plugin/gruvbox.rc.lua
@@ -22,10 +22,23 @@ require("gruvbox").setup({
   overrides = {
     ["@lsp.type.interface"] = { fg = "#f0e68c", italic = true, bold = true },
     ["@lsp.type.function"] = { fg = "#b16286", bold = true },
-    ["@lsp.type.method.go"] = { fg = "#b16286", bold = true },
+
     ["@method.call"] = { fg = "#b16286", bold = true },
+    ["@type.method"] = { fg = "#b16286", bold = true },
+
+    -- go
+    ["@lsp.type.method.go"] = { fg = "#b16286", bold = true },
     ["@function.method.call.go"] = { fg = "#b16286", bold = true },
     ["@function.method.go"] = { fg = "#b16286", bold = true },
+    ["@method.call.go"] = { fg = "#b16286", bold = true },
+
+    -- typescript
+    ["@lsp.type.method.typescript"] = { fg = "#b16286", bold = true },
+    ["@function.method.call.typescript"] = { fg = "#b16286", bold = true },
+    ["@function.method.typescript"] = { fg = "#b16286", bold = true },
+    ["@method.call.typescript"] = { fg = "#b16286", bold = true },
+
+    ["@lsp.type.method"] = { fg = "#b16286", bold = true },
   },
   dim_inactive = false,
   transparent_mode = true,
